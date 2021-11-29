9ee77db1e12a0c6e085cb2d7a766d18afb73efcf
Qiansha Meng <mengqianshasha@163.com>
2021-11-29 15:50:45 -0800

    update index.html, App.js, package-lock.json

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from "react";
 import {BrowserRouter, Route} from "react-router-dom";
+import './vendor/css/bootstrap.min.css'
+import './App.css';
 import HomeScreen from "./components/HomeScreen";
 import SearchScreen from "./components/SearchScreen";
 import ProfileScreen from "./components/ProfileScreen";
-import './vendor/css/bootstrap.min.css'
+import MessageScreen from "./components/MessageScreen";
+import AdminInboxScreen from "./components/AdminInboxScreen";
+import AdminInboxDetailScreen from "./components/AdminInboxDetailScreen";
+import BusinessClaimSearchScreen from "./components/BusinessClaimScreen/BusinessClaimSearchScreen";
 
 
 function App() {
   return (
       <BrowserRouter>
-        <Route path={"/"} exact={"true"}>
-          <HomeScreen/>
-        </Route>
-        <Route path={"/search"}>
-          <SearchScreen />
-        </Route>
-
-          <Route path="/profile" exact={true} component={ProfileScreen}/>
+          <Route path={["/", "/home", "/home/logged-in"]} exact={true} component={HomeScreen} />
+          <Route path={"/search"} exact={true} component={SearchScreen} />
+          <Route path={"/messages"} exact={true} component={MessageScreen} />
+          <Route path={"/admin/inbox"} exact={true} component={AdminInboxScreen} />
+          <Route path={"/admin/inbox/detail"} exact={true} component={AdminInboxDetailScreen} />
+          <Route path={"/business/claim"} component={BusinessClaimSearchScreen} />
+          <Route path={"/profile"} exact={true} component={ProfileScreen}/>
       </BrowserRouter>
 
   );

c191208eaed1e1e92f1712c847ed49a042aa53e6
Qiansha Meng <mengqianshasha@163.com>
2021-12-08 11:06:46 -0800

    add follow unfollow service

--- src/components/HomeScreen/index.js
+++ src/components/HomeScreen/index.js
@@ -6,12 +6,17 @@ import Footer from "./public-components/Footer";
 import BannerComponent from "./BannerComponent";
 import RecentActivityComponent from "./RecentActivityComponent";
 import {useDispatch, useSelector} from "react-redux";
-import {fetchProfile} from "../../services/profileService";
+import {fetchProfile, follow, unfollow} from "../../services/profileService";
 import isLoggedIn from "./utils/isLoggedIn";
 import defaultLocation from "./utils/defaultLocation.json";
 import {API_URL} from "../../CONST";
 
 const HomeScreen = () => {
+
+    const user1 = "61b05716bc403e9989444d61";
+    const user2 = "61b05759bc403e9989444d74";
+
+
     const dispatch = useDispatch();
 
     // Get profile
@@ -41,7 +46,16 @@ const HomeScreen = () => {
         setLocation(newLocation);
     }
 
-    // const use
+    // follow test
+
+    // const clickHandler = () => {
+    //     follow(user2, dispatch);
+    // }
+    //
+    //
+    // const unFollowHandler = () => {
+    //     unfollow(user2, dispatch);
+    // }
 
     return(
         <div className={"bg-white"}>
@@ -51,6 +65,17 @@ const HomeScreen = () => {
             <div className={"d-flex justify-content-center"}>
                 <RecentActivityComponent profile={profile}/>
             </div>
+
+            {/*<div className={"float-end pe-5"}>*/}
+            {/*    <button onClick={clickHandler}>*/}
+            {/*        {(profile !== undefined && Object.keys(profile).length !== 0 && profile['customerData']['followings'].includes(user2)) ? "unfollow" : "follow"}*/}
+            {/*    </button>*/}
+            {/*    <button onClick={unFollowHandler}>*/}
+            {/*        {(profile !== undefined && Object.keys(profile).length !== 0 && profile['customerData']['followings'].includes(user2)) ? "unfollow" : "follow"}*/}
+            {/*    </button>*/}
+
+            {/*</div>*/}
+
             <Footer />
         </div>
     );
